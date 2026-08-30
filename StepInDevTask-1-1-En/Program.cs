0ffe1868e2edf96bed32d3a875f5ae19cf833a82
Alexander <pchp250g8@tutamail.com>
2026-08-30 15:23:07 +0300

    Add project files.

---
+++ StepInDevTask-1-1-En/Program.cs
@@ -0,0 +1,12 @@
+﻿uint n; // number of blocks
+uint k; // number of towers
+Console.Clear(); // Clear screen
+Console.WriteLine("Enter the number of blocks");
+UInt32.TryParse(Console.ReadLine(),out n);
+Console.WriteLine("Enter the number of towers");
+UInt32.TryParse(Console.ReadLine(), out k);
+uint t = n / k; // Maximum number of towers
+uint b = t * k; // Number of blocks
+Console.WriteLine($"From {n} blocks, you can build a maximum of {t} towers");
+Console.WriteLine($"This will take {b} blocks.");
+Console.Read(); // Delay output until key "Enter" not pressed
